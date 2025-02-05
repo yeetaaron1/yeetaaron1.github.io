3fc6e81d53606b8c4b05e31c48d5750b5e2fe1dd
Aaron <cancholaron@gmail.com>
2025-02-04 22:50:39 -0800

    0.01-ALPHA

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import WindiCSS from "vite-plugin-windicss";
+import compression from "vite-plugin-compression";
+import viteImagemin from "vite-plugin-imagemin";
+import { VitePWA } from "vite-plugin-pwa";
+import AutoImport from "unplugin-auto-import/vite";
+//import { VitePluginNode } from "vite-plugin-node";
+
+// Change this to your GitHub repository name
+const repoName = "yeetaaron1.github.io"; 
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: `/${repoName}/`, // Set GitHub Pages base URL
+  plugins: [
+    react(),
+    WindiCSS(),
+    compression(),
+    viteImagemin({
+      mozjpeg: { quality: 75 },
+      pngquant: { quality: [0.7, 0.9] },
+      svgo: { plugins: [{ removeViewBox: false }] },
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "My Vite PWA",
+        short_name: "VitePWA",
+        theme_color: "#ffffff",
+      },
+    }),
+    AutoImport({
+      imports: ["react", "react-router-dom"],
+    }),
+    viteStaticCopy({
+      targets: [{ src: "public/*", dest: "" }],
+    }),
+    //...VitePluginNode({ adapter: "express", appPath: "./server/index.js" }),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist", // Output directory
+  },
+});
